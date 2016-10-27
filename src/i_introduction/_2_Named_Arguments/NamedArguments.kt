a8e0900da937e0c1791a94069347d1fa197ed594
Anne Costa <acosta@thoughtworks.com>
2016-10-27 11:22:28 -0200

    Removing unused method and puts task description as comment outside from method that do nothing.

--- src/i_introduction/_2_Named_Arguments/NamedArguments.kt
+++ src/i_introduction/_2_Named_Arguments/NamedArguments.kt
@@ -1,10 +1,11 @@
 package i_introduction._2_Named_Arguments
 
 /*
-        Task 2.
-        Implement the same logic as in 'task1' again through the library method 'joinToString()'.
-        Specify only two of the 'joinToString' arguments.
+    Task 2.
+    Implement the same logic as in 'task1' again through the library method 'joinToString()'.
+    Specify only two of the 'joinToString' arguments.
 */
+
 fun task2(collection: Collection<Int>): String {
     return collection.joinToString(", ", "{", "}")
 }

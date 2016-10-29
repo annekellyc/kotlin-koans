73896872ae66d871ecfcc24781b83d9975c6c9ca
Anne Kelly da Costa Geraldo <annekellyc@gmail.com>
2016-10-29 17:27:40 -0200

    Smart casts task.

--- src/i_introduction/_8_Smart_Casts/SmartCasts.kt
+++ src/i_introduction/_8_Smart_Casts/SmartCasts.kt
@@ -1,7 +1,9 @@
 package i_introduction._8_Smart_Casts
 
-import util.TODO
-import util.doc8
+/*
+    Task 8.
+    Rewrite 'JavaCode8.eval()' in Kotlin using smart casts and 'when' expression.
+ */
 
 interface Expr
 class Num(val value: Int) : Expr
@@ -9,16 +11,8 @@ class Sum(val left: Expr, val right: Expr) : Expr
 
 fun eval(e: Expr): Int =
         when (e) {
-            is Num -> todoTask8(e)
-            is Sum -> todoTask8(e)
+            is Num -> e.value
+            is Sum -> eval(e.left) + eval(e.right)
             else -> throw IllegalArgumentException("Unknown expression")
         }
-
-fun todoTask8(expr: Expr): Nothing = TODO(
-    """
-        Task 8.
-        Rewrite 'JavaCode8.eval()' in Kotlin using smart casts and 'when' expression.
-    """,
-    documentation = doc8(),
-    references = { JavaCode8().eval(expr) })
 
